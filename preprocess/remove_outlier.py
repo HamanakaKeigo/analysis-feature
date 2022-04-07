0266dbaeccf8335ec7aca5570c9bec1ed54df8c3
HamanakaKeigo <k-hamanaka@ist.osaka-u.ac.jp>
2022-04-07 16:17:13 +0900

    add time outlier

--- preprocess/remove_outlier.py
+++ preprocess/remove_outlier.py
@@ -11,6 +11,7 @@ def get_alldata(train_size,place):
         with open("../data/sites",'r') as f:
             sites = f.readlines()
             size = {}
+            time = {}
             for site in sites:
                 s = site.split()
                 if s[0] == "#":
@@ -18,6 +19,8 @@ def get_alldata(train_size,place):
                 path = "../data/dataset/origin/"+loc+"/"+s[1]+"/"
 
                 datasize = [0]*train_size
+                datatime = [0]*train_size
+
                 for i in range(train_size):
                     if not os.path.isfile(path +str(i)+ ".pcap"):
                         continue
@@ -29,13 +32,15 @@ def get_alldata(train_size,place):
                     
                     Size = np.abs(Size)
                     datasize[i] = sum(Size)
+                    datatime[i] = Time[-1]
                 
                 size[s[1]] = datasize
+                time[s[1]] = datatime
 
             size_list = np.array(list(size.values())).ravel()
-            return(size,size_list)
+            return(size,time,size_list)
 
-def remove(train_size,place,size):
+def remove(train_size,place,size,time):
 
     for loc in place:
         with open("../data/sites",'r') as f:
@@ -44,21 +49,27 @@ def remove(train_size,place,size):
                 s = site.split()
                 if s[0] == "#":
                     continue
-                data = size[s[1]]
 
-                q75, q25 = np.percentile(data, [75 ,25])
-                low = q25 - 1.5*(q75-q25)
-                high = q75 + 1.5*(q75-q25)
-                print(high,low)
-                
                 origin_path = "../data/dataset/origin/"+loc+"/"+s[1]+"/"
                 copy_path = "../data/dataset/processed/"+loc+"/"+s[1]
                 if not os.path.exists(copy_path):
                     os.makedirs(copy_path)
 
+                size75, size25 = np.percentile(size[s[1]], [75 ,25])
+                sizelow = size25 - 1.5*(size75-size25)
+                sizehigh = size75 + 1.5*(size75-size25)
+                print("size low,high",sizehigh,sizelow)
+
+                time75, time25 = np.percentile(time[s[1]], [75 ,25])
+                timelow = time25 - 1.5*(time75-time25)
+                timehigh = time75 + 1.5*(time75-time25)
+                print("size low,high",timehigh,timelow)
+                
                 for i in range(train_size):
-                    if (size[s[1]][i] < low or size[s[1]][i] > high):
-                        print("remove",origin_path,i,size[s[1]][i])
+                    if (size[s[1]][i] < sizelow or size[s[1]][i] > sizehigh):
+                        print("remove for size",origin_path,i,size[s[1]][i])
+                    else if(time[s[1]][i] < timelow or time[s[1]][i] > timehigh):
+                        print("remove for time",origin_path,i,time[s[1]][i])
                     else:
                         shutil.copy(origin_path+str(i)+".csv",copy_path)
               
@@ -69,6 +80,6 @@ if __name__ == "__main__":
     train_size=150
     place = ["icn"]
 
-    size,size_list = get_alldata(train_size,place)
+    size,time,size_list = get_alldata(train_size,place)
 
-    remove(train_size,place,size)
+    remove(train_size,place,size,time)
