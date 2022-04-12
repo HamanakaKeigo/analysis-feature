44d8dcee1b92a1146faa2bb4515e729e25c85f99
HamanakaKeigo <k-hamanaka@ist.osaka-u.ac.jp>
2022-04-12 16:13:46 +0900

    remake ptc

--- preprocess/ptc.py
+++ preprocess/ptc.py
@@ -7,14 +7,19 @@ import scipy.io
 import itertools
 import math
 import csv
+from scapy.all import *
 
 
-def to_csv(filename=""):
+def to_csv_pyshark(filename=""):
     print(filename)
 
     https = 443
     http  = 80
     data = pyshark.FileCapture(filename+".pcap")
+    with open(filename+".csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+
     out = [["index","size","time","ip"]]
 
 
@@ -24,9 +29,9 @@ def to_csv(filename=""):
         if "TCP" in packet:
             t = []
             t.append(i)
-            if(int(packet.tcp.dstport) == https or packet.tcp.dstport == http):
+            if(int(packet.tcp.dstport) == https or int(packet.tcp.dstport) == http):
                 t.append(int(packet.length))
-            elif(int(packet.tcp.srcport) == https or packet.tcp.srcport == http):
+            elif(int(packet.tcp.srcport) == https or int(packet.tcp.srcport) == http):
                 t.append(-int(packet.length))
             else:
                 continue
@@ -40,12 +45,47 @@ def to_csv(filename=""):
         writer = csv.writer(f)
         writer.writerows(out)
 
+def to_csv(filename=""):
+    print(filename)
+
+    https = 443
+    http  = 80
+    data = rdpcap(filename+".pcap")
+    with open(filename+".csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+
+    out = [["index","size","time","ip"]]
+
+
+    i=0
+    for packet in data:
+        #print(i)
+        if "TCP" in packet:
+            #print(packet.wirelen,packet.time,packet["IP"].src)
+            t = []
+            t.append(i)
+            if(int(packet["TCP"].dport) == https or int(packet["TCP"].dport) == http):
+                t.append(int(packet.wirelen))
+            elif(int(packet["TCP"].sport) == https or int(packet["TCP"].sport) == http):
+                t.append(-int(packet.wirelen))
+            else:
+                continue
+            t.append(packet.time)
+            t.append(packet["IP"].src)
+            out.append(t)
+            i+=1
+
+    with open(filename+".csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(out)
+
 if __name__ == "__main__":
     
-    loc = "icn"
+    loc = "icn2"
     train_size = 150
 
-    with open("../data/sites",'r') as f:
+    with open("../data/wsfsites",'r') as f:
         sites = f.readlines()
         for site in sites:
             s = site.split()
@@ -56,7 +96,7 @@ if __name__ == "__main__":
             features=[]
             for i in range(train_size):
                 if not os.path.isfile("../data/dataset/origin/"+loc+"/"+s[1]+"/"+str(i)+".pcap"):
-                    break
+                    continue
                 #print("../data/train/"+s[1]+"/"+str(i)+".pcap")
                 to_csv("../data/dataset/origin/"+loc+"/"+s[1]+"/"+str(i))
 
